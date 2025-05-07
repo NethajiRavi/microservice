69fa259cd8dfea469ee4aee69406fab5c31ebeb3
unknown <nethaji043@gmail.com>
2025-05-07 08:00:19 +0530

    first commit

---
+++ src/main/java/com/eazybytes/accounts/service/IAccountService.java
@@ -0,0 +1,12 @@
+package com.eazybytes.accounts.service;
+
+import com.eazybytes.accounts.dto.CustomerDto;
+
+public interface IAccountService {
+
+    /**
+     *
+     * @param customerDto -- customer Object
+     */
+
+    void createAccount(CustomerDto customerDto);}
---
+++ src/main/java/com/eazybytes/accounts/service/impl/AccountServiceImpl.java
@@ -0,0 +1,56 @@
+package com.eazybytes.accounts.service.impl;
+
+import com.eazybytes.accounts.constants.AccountsConstants;
+import com.eazybytes.accounts.dto.CustomerDto;
+import com.eazybytes.accounts.entity.Accounts;
+import com.eazybytes.accounts.entity.Customer;
+import com.eazybytes.accounts.exception.CustomerAlreadyExistsException;
+import com.eazybytes.accounts.mapper.CustomerMapper;
+import com.eazybytes.accounts.repository.AccountsRepository;
+import com.eazybytes.accounts.repository.CustomerRepository;
+import com.eazybytes.accounts.service.IAccountService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.Random;
+
+@Service
+@AllArgsConstructor
+public class AccountServiceImpl implements IAccountService {
+
+    private AccountsRepository accountsRepository;
+    private CustomerRepository customerRepository;
+
+
+
+    @Override
+    public void createAccount(CustomerDto customerDto) {
+        Customer customer = CustomerMapper.mapToCustomer(customerDto,new Customer());
+        Optional<Customer> existingCustomer =  customerRepository.findByMobileNumber(customerDto.getMobileNumber());
+        if(existingCustomer.isPresent()){
+            throw  new CustomerAlreadyExistsException("Customer Already Registered With Given Mobile Number"
+                    + customerDto.getMobileNumber());
+        }
+        customer.setCreatedAt(LocalDateTime.now());
+        customer.setCreatedBy("Anonymous");
+        accountsRepository.save(createNewAccount(customerRepository.save(customer)));
+    }
+
+
+    /**
+     * @param customer - Customer Object
+     * @return the new account details
+     */
+    private Accounts createNewAccount(Customer customer) {
+        Accounts newAccount = new Accounts();
+        newAccount.setCustomerId(customer.getCustomerId());
+        long randomAccNumber = 1000000000L + new Random().nextInt(900000000);
+
+        newAccount.setAccountNumber(randomAccNumber);
+        newAccount.setAccountType(AccountsConstants.SAVINGS);
+        newAccount.setBranchAddress(AccountsConstants.ADDRESS);
+        return newAccount;
+    }
+}
